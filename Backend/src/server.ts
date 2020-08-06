e7d0b676e95d7996a29c448a0452f48f8a94a479
Helena Paixão <hp.helenapaixao@gmail.com>
2020-08-06 14:35:39 -0400

    [FEATURE] - add knex and sqlite3

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -2,9 +2,8 @@ import express from 'express';
 
 const app = express();
 
-app.get('/users',() => {
-    console.log('Acessou a Rota')
-})
-
+app.post('/users', (request,response) => {
+  return response.json()
+});
 
 app.listen(3333);

c64e815a9506b9f37c08be0d0d34956d62bfeb73
Helena Paixão <hp.helenapaixao@gmail.com>
2020-08-06 19:11:08 -0400

    estilizando pagina

--- Frontend/web/src/pages/Landing/index.tsx
+++ Frontend/web/src/pages/Landing/index.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 import Logo from "../../assets/images/logo.svg";
+import LandingImg from "../../assets/images/landing.svg";
+import studyIcon from "../../assets/images/icons/study.svg";
+import giveClassesIcon from "../../assets/images/icons/give-classes.svg";
+import purpleHeartIcon from "../../assets/images/icons/give-classes.svg";
 
 function Landing() {
   return (
@@ -7,7 +11,26 @@ function Landing() {
       <div id="page-landing-content" className="container">
         <div className="logo-container">
           <img src={Logo} alt="logo" />
+          <h2>Sua plataforma de estudos online.</h2>
         </div>
+        
+
+        <img src={LandingImg} alt="landing" className="hero-image" />
+        <div className="buttons-container">
+          <a href="" className="study">
+            <img src={studyIcon} alt="study" />
+            Estudar
+          </a>
+
+          <a href="" className="give-classes">
+            <img src={giveClassesIcon} alt="study" />
+            Estudar
+          </a>
+        </div>
+            <span className="total-connections">
+                Total de 200 conexões já realizadas <img src={purpleHeartIcon} alt="coração"/>
+            </span>
+
       </div>
     </div>
   );

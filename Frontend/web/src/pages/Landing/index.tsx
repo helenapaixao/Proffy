378059a6dab467630a5d37a2e14347b4bdc60b32
Helena Paixão <hp.helenapaixao@gmail.com>
2020-08-06 19:14:02 -0400

    add css na pagina

--- Frontend/web/src/pages/Landing/index.tsx
+++ Frontend/web/src/pages/Landing/index.tsx
@@ -3,7 +3,7 @@ import Logo from "../../assets/images/logo.svg";
 import LandingImg from "../../assets/images/landing.svg";
 import studyIcon from "../../assets/images/icons/study.svg";
 import giveClassesIcon from "../../assets/images/icons/give-classes.svg";
-import purpleHeartIcon from "../../assets/images/icons/give-classes.svg";
+import purpleHeartIcon from "../../assets/images/icons/purple-heart.svg";
 
 function Landing() {
   return (
@@ -13,7 +13,6 @@ function Landing() {
           <img src={Logo} alt="logo" />
           <h2>Sua plataforma de estudos online.</h2>
         </div>
-        
 
         <img src={LandingImg} alt="landing" className="hero-image" />
         <div className="buttons-container">
@@ -27,10 +26,9 @@ function Landing() {
             Estudar
           </a>
         </div>
-            <span className="total-connections">
-                Total de 200 conexões já realizadas <img src={purpleHeartIcon} alt="coração"/>
-            </span>
-
+        <span className="total-connections">
+          Total de 200 conexões já realizadas <img src={purpleHeartIcon} alt="coração" />
+        </span>
       </div>
     </div>
   );

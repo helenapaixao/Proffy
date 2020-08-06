839ec7dbf2cd9abc844dd46df7c2463f3ea0153a
Helena Paixão <hp.helenapaixao@gmail.com>
2020-08-06 18:09:22 -0400

    backend almost done

--- Backend/src/controllers/ConnectionsController.ts
+++ Backend/src/controllers/ConnectionsController.ts
@@ -1,9 +1,14 @@
-import { ConnectionConfig } from "knex";
+
 import { Response, Request } from "express";
 import db from "../database/connection";
 
 export default class ConnectionController {
-  async index(request: Request, response: Response) {}
+  async index(request: Request, response: Response) {
+      const totalConnections = await db('connections').count('* as total');
+      const { total} = totalConnections[0]
+        return response.json({total})
+
+  }
 
   async create(request: Request, response: Response) {
     const { user_id } = request.body;

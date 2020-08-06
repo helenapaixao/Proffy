6a394b0bc157eaf18c8b76c0f6208b2440662858
Helena Paixão <hp.helenapaixao@gmail.com>
2020-08-06 17:08:19 -0400

    [FEATURE] add MVC

--- Backend/src/routes.ts
+++ Backend/src/routes.ts
@@ -1,57 +1,10 @@
 import express from "express";
-import db from "./database/connection";
-import convertHourToMinutes from "./utils/convertHourToMinutes";
+
 
 const routes = express.Router();
 
-interface ScheduleItem {
-  week_day: number;
-  from: string;
-  to: string;
-}
 
-routes.post("/classes", async (request, response) => {
-  const { name, avatar, bio, whatsapp, subject, cost, schedule } = request.body;
 
-  const trx = await db.transaction();
-
-  try {
-    const insertedUsersIds = await trx("users").insert({
-      name,
-      avatar,
-      bio,
-      whatsapp,
-    });
-
-    const user_id = insertedUsersIds[0];
-
-    const insertedClassesIds = await trx("classes").insert({
-      subject,
-      cost,
-      user_id,
-    });
-
-    const class_id = insertedClassesIds[0];
-
-    const classSchedule = schedule.map((scheduleItem: ScheduleItem) => {
-      return {
-        class_id,
-        week_day: scheduleItem.week_day,
-        from: convertHourToMinutes(scheduleItem.from),
-        to: convertHourToMinutes(scheduleItem.to),
-      };
-    });
-
-    await trx("class_schedule").insert(classSchedule);
-
-    await trx.commit();
-
-    return response.send();
-  } catch (error) {
-    return response.status(400).json({
-      error: "Unexpected error while creating new class",
-    });
-  }
-});
+routes.post("/classes", )
 
 export default routes;

5d79e3ffa3c9a1b8ddb00686019fd64d6c171a31
Helena Paixão <hp.helenapaixao@gmail.com>
2020-08-06 16:08:33 -0400

    [FEATURE] - add convertHourToMinutes

--- Backend/src/routes.ts
+++ Backend/src/routes.ts
@@ -3,14 +3,36 @@ import db from "./database/connection";
 
 const routes = express.Router();
 
-routes.post("/classes", async (request, response) => {
-  const { name, avatar, bio,whatsapp, subject, cost, schedule } = request.body;
+interface ScheduleItem {
+  week_day: number;
+  from: string;
+  to: string;
+}
 
-  await db("users").insert({
-      name,
-      avatar,
-      bio,
-      whatsapp,
+routes.post("/classes", async (request, response) => {
+  const { name, avatar, bio, whatsapp, subject, cost, schedule } = request.body;
+
+  const insertedUsersIds = await db("users").insert({
+    name,
+    avatar,
+    bio,
+    whatsapp,
+  });
+
+  const user_id = insertedUsersIds[0];
+
+  const insertedClassesIds = await db("classes").insert({
+    subject,
+    cost,
+    user_id,
+  });
+
+  const class_id = insertedClassesIds[0];
+  const clasSchedule = schedule.map((scheduleItem: ScheduleItem) => {
+    return {
+      week_day: scheduleItem.week_day,
+      from:
+    };
   });
 
   return response.send();

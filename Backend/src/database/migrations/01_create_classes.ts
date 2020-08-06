7ab5f689d26b04d2f7a398d74f3e26bb2312c3f8
Helena Paixão <hp.helenapaixao@gmail.com>
2020-08-06 15:11:16 -0400

    [FEATURE] - create table classes

--- Backend/src/database/migrations/01_create_classes.ts
+++ Backend/src/database/migrations/01_create_classes.ts
@@ -3,10 +3,10 @@ import Knex from "knex";
 export async function up(knex: Knex) {
   return knex.schema.createTable("users", (table) => {
     table.increments("id").primary();
-    table.string("name").notNullable();
-    table.string("avatar").notNullable();
-    table.string("whatsapp").notNullable();
-    table.string("bio").notNullable();
+    table.string("subject").notNullable();
+    table.string("cost").notNullable();
+
+    table.integer("user_id").notNullable().references("id").inTable("users").onDelete("CASCADE");
   });
 }
 

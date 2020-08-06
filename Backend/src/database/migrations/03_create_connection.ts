2a477dd319ebe88856224a7e2b12e6baf73d5468
Helena Paixão <hp.helenapaixao@gmail.com>
2020-08-06 15:30:48 -0400

    tables done

--- Backend/src/database/migrations/03_create_connection.ts
+++ Backend/src/database/migrations/03_create_connection.ts
@@ -1,23 +1,21 @@
 import Knex from "knex";
 
 export async function up(knex: Knex) {
-  return knex.schema.createTable("class_schedule", (table) => {
+  return knex.schema.createTable("connections", (table) => {
     table.increments("id").primary();
 
-    table.integer("week_day").notNullable();
-    table.integer("from").notNullable();
-    table.integer("to").notNullable();
-
     table
-      .integer("class_id")
+      .integer("user")
       .notNullable()
       .references("id")
       .inTable("users")
       .onUpdate("CASCADE")
       .onDelete("CASCADE");
+
+    table.timestamp("created_at").defaultTo("now()").notNullable();
   });
 }
 
 export async function down(knex: Knex) {
-  return knex.schema.dropTable("useclass_schedulers");
+  return knex.schema.dropTable("connections");
 }

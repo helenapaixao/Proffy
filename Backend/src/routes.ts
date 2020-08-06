8ee56818764b9d77848a89c46fab3ab260d834a6
Helena Paixão <hp.helenapaixao@gmail.com>
2020-08-06 17:20:49 -0400

    [FEATURE] add inner join

--- Backend/src/routes.ts
+++ Backend/src/routes.ts
@@ -1,10 +1,10 @@
 import express from "express";
-
+import ClassesController from "./controllers/ClassesController";
 
 const routes = express.Router();
+const classesController = new ClassesController();
 
-
-
-routes.post("/classes", )
+routes.post("/classes", classesController.create);
+routes.get("/classes", classesController.index);
 
 export default routes;

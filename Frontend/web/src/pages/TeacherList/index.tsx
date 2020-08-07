dca48b599eff6294e512fd89afd21033be8a630b
Helena Paixão <hp.helenapaixao@gmail.com>
2020-08-07 17:45:12 -0400

    cards ok

--- Frontend/web/src/pages/TeacherList/index.tsx
+++ Frontend/web/src/pages/TeacherList/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "../../components/PageHeader";
 import "./styles.css";
+import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 function TeacherList() {
   return (
@@ -23,6 +24,31 @@ function TeacherList() {
           </div>
         </form>
       </Header>
+      <main>
+        <article className="teacher-item">
+          <header>
+            <img src="https://avatars0.githubusercontent.com/u/11083288?s=460&u=3548efa4b019bb0c4041dfdeeff5f510bbcce99e&v=4" alt="Helena Paixão" />
+            <div>
+              <strong>Helena Paixão</strong>
+              <span>Quimica</span>
+            </div>
+          </header>
+            <p>
+              Entusiasta das melhores tecnologias
+            </p>
+            <footer>
+              <p>
+                Preço/hora
+                <strong>R$ 80,00</strong>
+              </p>
+              <button type="button">
+                <img src={whatsappIcon} alt="whats"/>
+                Entrar em contato
+
+              </button>
+            </footer>
+        </article>
+      </main>
     </div>
   );
 }

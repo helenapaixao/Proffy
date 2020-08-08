23871fe3d948b65ab788f7db5c93868be8b8622c
Helena Paixão <hp.helenapaixao@gmail.com>
2020-08-08 18:28:15 -0400

    integração com b back-end

--- Frontend/web/src/pages/Landing/index.tsx
+++ Frontend/web/src/pages/Landing/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState,useEffect } from "react";
 import { Link } from "react-router-dom";
 
 import logo from "../../assets/images/logo.svg";
@@ -8,8 +8,18 @@ import studyIcon from "../../assets/images/icons/study.svg";
 import giveClassesIcon from "../../assets/images/icons/give-classes.svg";
 import purpleHeartIcon from "../../assets/images/icons/purple-heart.svg";
 import "./style.css";
+import api from "../../services/api";
 
 function Landing() {
+  const [totalConnections,setTotalConnections] = useState(0);
+
+  useEffect(() => {
+    api.get('/connections').then(response => {
+      console.log(response)
+    })
+  },[])
+
+
   return (
     <div id="page-landing">
       <div id="page-landing-content" className="container">

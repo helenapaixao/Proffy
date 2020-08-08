fbde36c84cdffc68f479fafc1123b51b8c5304c3
Helena Paixão <hp.helenapaixao@gmail.com>
2020-08-08 13:19:21 -0400

    config form

--- Frontend/web/src/pages/TeacherForm/index.tsx
+++ Frontend/web/src/pages/TeacherForm/index.tsx
@@ -7,6 +7,15 @@ import Textarea from "../../components/Textarea";
 import Select from "../../components/Select";
 
 function TeacherForm() {
+  const scheduleItems = [
+    { week_day: 0, from: "8:00 AM", to: "16:00 PM" },
+    { week_day: 0, from: "8:00 AM", to: "16:00 PM" },
+  ];
+
+  function addNewScheduleItem() {
+    console.log("teste");
+  }
+
   return (
     <div id="page-teacher-form" className="container">
       <Header
@@ -41,23 +50,31 @@ function TeacherForm() {
         <fieldset>
           <legend>
             Horarios disponíveis
-            <button type="button">+ Novo horário</button>
+            <button type="button" onClick={addNewScheduleItem}>
+              + Novo horário
+            </button>
           </legend>
-          <div className="schedule-item">
-          <Select name="week_day" label="Dia da semana" options={[
-          {value: '0',label:'Domingo'},
-          {value: '1',label:'Segunda'},
-          {value: '2',label:'Terça'},
-          {value: '3',label:'Quarta'},
-          {value: '4',label:'Quinta'},
-          {value: '5',label:'Sexta'},
-          {value: '6',label:'Sabado'},
-
-          ]} />
-          <Input name="from" label="Das" type="time"/>
-          <Input name="to" label="Até" type="time"/>
-
-          </div>
+          {scheduleItems.map((scheduleItem) => {
+            return (
+              <div className="schedule-item">
+                <Select
+                  name="week_day"
+                  label="Dia da semana"
+                  options={[
+                    { value: "0", label: "Domingo" },
+                    { value: "1", label: "Segunda" },
+                    { value: "2", label: "Terça" },
+                    { value: "3", label: "Quarta" },
+                    { value: "4", label: "Quinta" },
+                    { value: "5", label: "Sexta" },
+                    { value: "6", label: "Sabado" },
+                  ]}
+                />
+                <Input name="from" label="Das" type="time" />
+                <Input name="to" label="Até" type="time" />
+              </div>
+            );
+          })}
         </fieldset>
         <footer>
           <p>
